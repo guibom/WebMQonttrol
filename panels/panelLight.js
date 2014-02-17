1cebe48a93a4612d9a37a9870b32a41f9f1de0d1
guibom <longinus@gmail.com>
2014-02-17 18:48:30 -0500

    Set defaults for lights

--- panels/panelLight.js
+++ panels/panelLight.js
@@ -8,13 +8,25 @@ var panelLight = {
     type: "Light",
     $panel: $("#Controller_Light"),
 
+    //Constructor, run once at start
+    __begin__: function(){
+        //Set all lights to off by default
+        $(".WMQ_light").html('<span class="glyphicon glyphicon-remove"></span> OFF').removeClass('btn-success').addClass('btn-danger');
+    },
+
     "home/light/1/status": function() {
         if (msg.payload == 'on') {
-            $('#Light_1').html('<span class="glyphicon glyphicon-ok"></span> ON');                        
-            $('#Light_1').removeClass('btn-danger').addClass('btn-success');
+            this.LightON($('#Light_1'));
         } else {
-            $('#Light_1').html('<span class="glyphicon glyphicon-remove"></span> OFF');  
-            $('#Light_1').removeClass('btn-success').addClass('btn-danger');
+            this.LightOFF($('#Light_1'));
+        }
+    },
+
+    "home/light/2/status": function() {
+        if (msg.payload == 'on') {
+            this.LightON($('#Light_2'));
+        } else {
+            this.LightOFF($('#Light_2'));
         }
     },
 
@@ -23,6 +35,17 @@ var panelLight = {
         $('#slider1').val(parseInt(msg.payload));
     },
 
+    LightON: function($controller) {
+        $controller.html('<span class="glyphicon glyphicon-ok"></span> ON');                        
+        $controller.removeClass('btn-danger').addClass('btn-success');
+    },
+
+    LightOFF: function($controller) {       
+        $controller.html('<span class="glyphicon glyphicon-remove"></span> OFF');  
+        $controller.removeClass('btn-success').addClass('btn-danger'); 
+    },
+
+    //Generic Click action redirected here from the ID
     Click: function(controller) {
         //Get light number from jquery object's id
         var light_id = controller.attr("id").split("_");
