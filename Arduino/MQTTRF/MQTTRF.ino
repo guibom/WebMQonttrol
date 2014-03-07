ca6d9103b3d38339f258d8eb655ec003eff370ce
guibom <longinus@gmail.com>
2014-03-06 23:02:48 -0500

    Removed strings from code

--- Arduino/MQTTRF/MQTTRF.ino
+++ Arduino/MQTTRF/MQTTRF.ino
@@ -4,8 +4,9 @@
 // Released under the MIT license. See LICENSE file for details.
 
 //If true, prints debug messages to serial
-#define DEBUG     true
-#define DEBUG_MEM false
+#define DEBUG       false
+#define DEBUG_MEM   false
+#define PUBLISH_MEM false
 
 #include <JsonParser.h>
 #include <SPI.h>
@@ -16,9 +17,7 @@
 #include <x10constants.h>
 #include <avr/pgmspace.h>
 #include <Streaming.h>
-#if DEBUG_MEM
-  #include <MemoryFree.h>
-#endif
+#include <MemoryFree.h>
 
 //IO Pin Constants
 #define STATUS_LED      A5
@@ -28,12 +27,15 @@
 //Set up a new x10 instance
 x10 myHouse =  x10(X10_ZERO_CROSS, X10_DATA);
 
-//Buffer for messages
+//X10 Json parser
+JsonParser<12> parser;
+
+//Buffer for messages/progmem topics
 char buffer[64];
-char buffer_topic[64];
 
 #define STR_ON  "on"
 #define STR_OFF "off"
+#define STR_1   "1"
 #define STR_0   "0"
 #define STR_100 "100"
 
@@ -42,6 +44,8 @@ char buffer_topic[64];
 #define STR_GATEWAY_L1    1
 #define STR_L1_STATUS     2
 #define STR_L1_BRIGHTNESS 3
+#define STR_X10_STATUS    4
+#define STR_GATEWAY_RAM   5
 
 //PROGMEM Topics
 prog_char P_STR_GATEWAY[] PROGMEM = {"home/gateway/MQTTRF"};
@@ -50,6 +54,10 @@ prog_char P_STR_GATEWAY_L1[] PROGMEM = {"home/gateway/MQTTRF/light/1"};
 prog_char P_STR_L1_STATUS[] PROGMEM = {"home/light/1/status"};
 prog_char P_STR_L1_BRIGHTNESS[] PROGMEM = {"home/light/1/status/brightness"};
 
+prog_char P_STR_X10_STATUS[] PROGMEM = {"home/x10/status"};
+
+prog_char P_STR_GATEWAY_RAM[] PROGMEM = {"home/gateway/MQTTRF/memory/free"};
+
 //Array that holds all the PROGMEM topic strings
 PROGMEM const char *string_table[] =
 {   
@@ -57,18 +65,25 @@ PROGMEM const char *string_table[] =
   P_STR_GATEWAY_L1,     //1
   P_STR_L1_STATUS,      //2
   P_STR_L1_BRIGHTNESS,  //3
+  P_STR_X10_STATUS,     //4
+  P_STR_GATEWAY_RAM     //5
 };
 
+const char STR_AA[] PROGMEM = "2";
+
+
 //PROGMEM Subscription topics. Automatically subscribe to all of them at the start
-#define STR_TOPICS_SIZE 2  //Needs to reflect the actual number of topics needed
+#define STR_TOPICS_SIZE 3  //Needs to reflect the actual number of topics needed
 prog_char P_STR_SUB_L_ALL[] PROGMEM = {"home/light/all/#"};
 prog_char P_STR_SUB_L_1[] PROGMEM = {"home/light/1/#"};
+prog_char P_STR_SUB_X10[] PROGMEM = {"home/x10/action"};
 
 //Holds all the subscriptions.
 PROGMEM const char *string_topics[] =
 {   
   P_STR_SUB_L_ALL,
-  P_STR_SUB_L_1
+  P_STR_SUB_L_1,
+  P_STR_SUB_X10
 };
 
 //PROGMEM strings for MESSAGES
@@ -86,42 +101,35 @@ byte ip[]     = { 192, 168, 0, 20 };
 // Callback function header
 void callback(char* topic, byte* payload, unsigned int length);
 
+//Ethernet and MQTT client
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
 
-//Json
-JsonParser<12> parser;
 
-// Callback function
 void callback(char* topic, byte* payload, unsigned int length) {
 
-    int i = 0;
+  //Convert payload to char array
+  char *cpayload = (char *) payload;
+  cpayload[length] = '\0';
+
+  #if DEBUG 
+    Serial << "Message received: [" << topic << "] [" << cpayload << "]" << endl;
+  #endif
+  #if DEBUG_MEM
+    Serial << "MEM: " << freeMemory() << endl;
+  #endif
+
+  //Light 1 Actions
+  if (strncmp(topic,"home/light/1/", 13)==0) {
+
     byte lightId, lightCmd, lightMax, lightVal;
-
-    //Add payload to message buffer string
-    for(i=0; i<length; i++) {
-        buffer[i] = payload[i];
-    }
-           
-    buffer[i] = '\0';
-    String msgString = String(buffer);
-
-    #if DEBUG            
-      //Serial.print("Message received: "); Serial.print(topic); Serial.print(" "); Serial.println(msgString);
-      Serial << "Message received: " << topic << " " << msgString << endl;
-    #endif
-    #if DEBUG_MEM
-      Serial << "MEM: " << freeMemory() << endl;
-      // Serial.print("MEM: "); Serial.println(freeMemory());
-    #endif
-
     //Just one light for now, hardcoded values
-    lightId = 0xA1;      
+    lightId = 0xA1;
 
-    //Light 1, ON
+    //ON    
     if (strcmp(topic,"home/light/1/on")==0) {      
       //Check for message
-      if (msgString.equals("1")) {
+      if (strcmp(cpayload, "1")==0) {
         lightCmd = 0x10;
         lightUpdate(lightId, lightCmd, 100);
         
@@ -131,10 +139,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
       }
     }
 
-    //Light 1, Off
+    //OFF
     if (strcmp(topic,"home/light/1/off")==0) {
       //Check for message
-      if (msgString.equals("1")) {
+      if (strcmp(cpayload, "1")==0) {
         lightCmd = 0x10;
         lightUpdate(lightId, lightCmd, 0);
 
@@ -143,10 +151,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
       }
     }
 
-    //Light 1, Set
+    //SET
     if (strcmp(topic,"home/light/1/set")==0) {
       lightCmd = 0x10;
-      int bvalue = msgString.toInt();
+      int bvalue = atoi(cpayload);
 
       //Clamp payload to safe numbers
       if (bvalue > 100)
@@ -171,53 +179,61 @@ void callback(char* topic, byte* payload, unsigned int length) {
       Publish(STR_L1_BRIGHTNESS, b_payload, true);
     }
 
-    //Light 1, Fade_on
+    //FADE_ON
     if (strcmp(topic,"home/light/1/fade_on")==0) {
       lightCmd = 0x20;
-      lightUpdate(lightId, lightCmd, msgString.toInt(), msgString.toInt());
+      lightUpdate(lightId, lightCmd, atoi(cpayload), atoi(cpayload));
 
       Publish(STR_L1_STATUS, STR_ON, true);
       Publish(STR_L1_BRIGHTNESS, STR_100, true);
     }
 
-    //Light 1, Fade_off
+    //FADE_OFF
     if (strcmp(topic,"home/light/1/fade_off")==0) {
       lightCmd = 0x30;
-      lightUpdate(lightId, lightCmd, msgString.toInt(), msgString.toInt());
+      lightUpdate(lightId, lightCmd, atoi(cpayload), atoi(cpayload));
       
       Publish(STR_L1_STATUS, STR_OFF, true);
       Publish(STR_L1_BRIGHTNESS, STR_0, true);
     }
+  }
 
-    //X10
-    if (strcmp(topic,"home/x10/action")==0) {           
-    //      String house = getValue(msgString, ',', 0);
-    //      String unit = getValue(msgString, ',', 1);
-    //      String action = getValue(msgString, ',', 2);
-    //      String tries = getValue(msgString, ',', 3);      
-    //      
-    //       char charBuf[30];  
-    //       msgString.toCharArray(charBuf, 30);
-    //      
-    //      JsonHashTable hashTable = parser.parseHashTable(charBuf);
-    //  
-    //      if (!hashTable.success())
-    //      {
-    //          Serial.println("JsonParser.parseHashTable() failed");
-    //          return;
-    //      }
-    //
-    //      char charBuf[10];     
-    //      msgString.toCharArray(charBuf, 10);          
-    //    
-    //      //Check for message
-    //      myHouse.write(house.toInt(), unit.toInt(), tries.toInt());
-    //      myHouse.write(house.toInt(), action.toInt(), tries.toInt());
-    //      
-    //      client.publish("home/x10/status", charBuf);
+  //X10
+  if (strncmp(topic,"home/x10/action", 15)==0) {
 
-      //{"H":0,"U":2,"A":99,"R":3}
+    //Copy payload first    
+    char JsonACK[strlen(cpayload)+1];    
+    strcpy (JsonACK,cpayload);
+
+    //Parse JSON payload -- Format {"H":0,"U":2,"A":99,"T":3}
+    JsonHashTable hashTable = parser.parseHashTable(cpayload);
+
+    //Failed (bigger then buffer?)
+    if (!hashTable.success()) {
+      #if DEBUG                
+        Serial << "JSON Parser failed!" << endl;
+      #endif
+      return;
     }
+
+    //Parsed JSON OK!
+    #if DEBUG                
+      Serial << "X10 Action: " << hashTable.getLong("H") << " " << hashTable.getLong("U") << " " << hashTable.getLong("A") << " " << hashTable.getLong("T") << endl;
+    #endif
+
+    //Execute X10 Command
+    //myHouse.write(hashTable.getLong("H"), hashTable.getLong("U"), hashTable.getLong("T"));
+    //myHouse.write(hashTable.getLong("H"), hashTable.getLong("A"), hashTable.getLong("T"));
+     
+    //Acknowledge X10 status by publishing the same message 
+    Publish(STR_X10_STATUS, JsonACK, true);
+  }
+
+  //Publish ammount of free memory
+  #if PUBLISH_MEM
+      Publish_FreeMem();
+  #endif
+
 }
 
 //Maxium val on lightMax
@@ -262,13 +278,14 @@ void setup() {
 
   //Try to connect to MQTT server        
   if (client.connect("MQTTRF", "home/gateway/MQTTRF", 0, 1, STR_OFF)) {
+
     #if DEBUG    
-      Serial.println("Connected to MQTT broker");
+        Serial.println("Connected to MQTT broker");
     #endif
 
     //Publish gateway abilities and conencted hardware    
     Publish(STR_GATEWAY, STR_ON, true);
-    Publish(STR_GATEWAY_L1, "1", true);    
+    Publish(STR_GATEWAY_L1, STR_1, true);    
           
     //Subscribe to topics defined in PROGMEM array
     subscribeTopics();
@@ -276,8 +293,24 @@ void setup() {
     //Set status light to ON
     digitalWrite(STATUS_LED, LOW);
 
+    //RF433 Setup
+    vw_set_tx_pin(2);
+    vw_set_rx_pin(5);
+    vw_set_ptt_pin(1);
+    vw_setup(1000);  // Bits per sec
+    
+    #if DEBUG_MEM
+        Serial << "MEM: " << freeMemory() << endl;
+    #endif
+
+    //Publish ammount of free memory
+    #if PUBLISH_MEM
+        Publish_FreeMem();
+    #endif
+
+  //Failed to connect to server
   } else {
-    //Failed to connect to server
+    
     #if DEBUG    
       Serial.println("Failed to connect to MQTT broker!");
     #endif
@@ -290,16 +323,6 @@ void setup() {
       delay(250);
     }
   }
-
-  //RF433 Setup
-  vw_set_tx_pin(2);
-  vw_set_rx_pin(5);
-  vw_set_ptt_pin(1);
-  vw_setup(1000);  // Bits per sec
-  
-  #if DEBUG_MEM
-      Serial.print("MEM: "); Serial.println(freeMemory());
-  #endif
 }
 
 //Default loop, just process MQTT messages
@@ -310,45 +333,58 @@ void loop() {
 //Subscribe to list of topics in PROGMEM
 void subscribeTopics() {
 
+  //Loop over all the topics defined in PROGMEM
   for (int i = 0; i < STR_TOPICS_SIZE; i++) {
-    strcpy_P(buffer_topic, (char*)pgm_read_word(&(string_topics[i])));    
-    client.subscribe(buffer_topic);  //MQTT Subscribe
+    strcpy_P(buffer, (char*)pgm_read_word(&(string_topics[i])));    
+    client.subscribe(buffer);  //MQTT Subscribe
 
-    #if DEBUG    
-        Serial.print("Subscribed to topic: "); Serial.println(buffer_topic);
+    #if DEBUG            
+        Serial << "Subscribed to topic: " << buffer << endl;
     #endif
   }  
+
 }
 
 char *returnTopic(uint8_t i) {    
-    strcpy_P(buffer_topic, (char*)pgm_read_word(&(string_table[i])));
-    return buffer_topic;
+    strcpy_P(buffer, (char*)pgm_read_word(&(string_table[i])));
+    return buffer;
 }
 
-//Uses topic in PROGMEM
+//Uses topic in PROGMEM, autosize payload
 boolean Publish(uint8_t topic, char* payload, boolean retained) {
   
   //Publish message
-  client.publish(returnTopic(topic), (uint8_t*)payload, strlen(payload), retained); 
+  bool res = client.publish(returnTopic(topic), (uint8_t*)payload, strlen(payload), retained); 
 
   #if DEBUG
-      //Add payload to message buffer string
-      int i;
-      for(i=0; i<strlen(payload); i++)
-          buffer[i] = payload[i];
-             
-      buffer[i] = '\0';
-      String msgString = String(buffer);
-
-      Serial.print("Message sent: "); Serial.print(returnTopic(topic)); Serial.print(" "); Serial.println(msgString);
+      Serial << "Message sent: [" << returnTopic(topic) << "] [" << payload << "]" << endl;
   #endif
-  #if DEBUG_MEM
-      Serial.print("MEM: "); Serial.println(freeMemory());
+  #if DEBUG_MEM      
+      Serial << "MEM: " << freeMemory() << endl;
   #endif
 
+  //Publish ammount of free memory
+  // #if PUBLISH_MEM
+  //     Publish_FreeMem();
+  // #endif
+
+  return res;
+}
+
+//Publish the amount of Free Memory to MQTT
+boolean Publish_FreeMem() {
+  char mem[4];
+  sprintf(mem, "%d", freeMemory());  
+
+  return client.publish(returnTopic(STR_GATEWAY_RAM), (uint8_t*)mem, strlen(mem), true);
 }
 
 //Return size of PROGMEM variable
 byte size(prog_uchar *var) {
   return sizeof(var)-1;
+}
+
+char* getString(const char* str) {
+  strcpy_P(buffer, (char*)str);
+  return buffer;
 }
